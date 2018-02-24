d39b923f6458dccef81df8cbd16fab7fc6b462bb
Scott <scott.lee.roberts@gmail.com>
2018-02-23 21:57:07 -0500

    Scope posts to current author

--- app/controllers/authors/posts_controller.rb
+++ app/controllers/authors/posts_controller.rb
@@ -2,21 +2,21 @@ class Authors::PostsController < AuthorController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.most_recent
+    @posts = current_author.posts.most_recent
   end
 
   def show
   end
 
   def new
-    @post = Post.new
+    @post = current_author.posts.new
   end
 
   def edit
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_author.posts.new(post_params)
 
     respond_to do |format|
       if @post.save
@@ -46,7 +46,7 @@ class Authors::PostsController < AuthorController
 
   private
   def set_post
-    @post = Post.friendly.find(params[:id])
+    @post = current_author.posts.friendly.find(params[:id])
   end
 
   def post_params
