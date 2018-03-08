0a0b74df99139baaf87b8e2d20e7cc9cb5f26116
Scott <scott.lee.roberts@gmail.com>
2018-03-07 21:00:30 -0500

    Seeds data has posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-#
 Designer.create(name: 'user', email: 'user@email.com', password: 'password', password_confirmation: 'password')
 Designer.create(name: 'user22', email: 'user2@email.com', password: 'password', password_confirmation: 'password')
 
+Designer.first.posts << Post.new(title: 'Subway tile tumblr wayfarers keffiyeh',
+                                 body: "Lorem ipsum dolor amet shoreditch lyft stumptown etsy copper mug post-ironic, pop-up normcore. Actually iPhone squid, plaid drinking vinegar synth mlkshk fashion axe brooklyn disrupt viral 8-bit. Microdosing locavore semiotics ethical lo-fi freegan salvia tumblr YOLO. Jianbing truffaut actually seitan humblebrag, 8-bit church-key yr. Gentrify chillwave jean shorts tousled health goth umami street art hoodie asymmetrical you probably haven't heard of them blog everyday carry tumeric kinfolk shaman. Prism food truck skateboard health goth art party seitan polaroid messenger bag umami lomo kinfolk squid asymmetrical lyft knausgaard. Cronut offal pug keytar.",
+                                 description: 'Microdosing locavore semiotics ethical lo-fi freegan salvia tumblr YOLO. Jianbing truffaut actually seitan humblebrag, 8-bit church-key yr',
+                                 banner_image_url: 'https://images.pexels.com/photos/690303/pexels-photo-690303.jpeg?w=940&h=650&auto=compress&cs=tinysrgb')
+
+Designer.first.posts << Post.new(title: 'Cliche mixtape cred readymade small batch',
+                                 body: "Lorem ipsum dolor amet shoreditch lyft stumptown etsy copper mug post-ironic, pop-up normcore. Actually iPhone squid, plaid drinking vinegar synth mlkshk fashion axe brooklyn disrupt viral 8-bit. Microdosing locavore semiotics ethical lo-fi freegan salvia tumblr YOLO. Jianbing truffaut actually seitan humblebrag, 8-bit church-key yr. Gentrify chillwave jean shorts tousled health goth umami street art hoodie asymmetrical you probably haven't heard of them blog everyday carry tumeric kinfolk shaman. Prism food truck skateboard health goth art party seitan polaroid messenger bag umami lomo kinfolk squid asymmetrical lyft knausgaard. Cronut offal pug keytar.",
+                                 description: 'Microdosing locavore semiotics ethical lo-fi freegan salvia tumblr YOLO. Jianbing truffaut actually seitan humblebrag, 8-bit church-key yr',
+                                 banner_image_url: 'https://images.pexels.com/photos/918327/pexels-photo-918327.jpeg?w=940&h=650&auto=compress&cs=tinysrgb')
+
+Designer.first.posts << Post.new(title: 'Lorem  dolor amet shoreditch',
+                                 body: "Lorem ipsum dolor amet shoreditch lyft stumptown etsy copper mug post-ironic, pop-up normcore. Actually iPhone squid, plaid drinking vinegar synth mlkshk fashion axe brooklyn disrupt viral 8-bit. Microdosing locavore semiotics ethical lo-fi freegan salvia tumblr YOLO. Jianbing truffaut actually seitan humblebrag, 8-bit church-key yr. Gentrify chillwave jean shorts tousled health goth umami street art hoodie asymmetrical you probably haven't heard of them blog everyday carry tumeric kinfolk shaman. Prism food truck skateboard health goth art party seitan polaroid messenger bag umami lomo kinfolk squid asymmetrical lyft knausgaard. Cronut offal pug keytar.",
+                                 description: 'Microdosing locavore semiotics ethical lo-fi freegan salvia tumblr YOLO. Jianbing truffaut actually seitan humblebrag, 8-bit church-key yr',
+                                 banner_image_url: 'https://images.pexels.com/photos/690303/pexels-photo-690303.jpeg?w=940&h=650&auto=compress&cs=tinysrgb')
+
+Designer.first.posts << Post.new(title: 'Four dollar toast kogi next level',
+                                 body: "Lorem ipsum dolor amet shoreditch lyft stumptown etsy copper mug post-ironic, pop-up normcore. Actually iPhone squid, plaid drinking vinegar synth mlkshk fashion axe brooklyn disrupt viral 8-bit. Microdosing locavore semiotics ethical lo-fi freegan salvia tumblr YOLO. Jianbing truffaut actually seitan humblebrag, 8-bit church-key yr. Gentrify chillwave jean shorts tousled health goth umami street art hoodie asymmetrical you probably haven't heard of them blog everyday carry tumeric kinfolk shaman. Prism food truck skateboard health goth art party seitan polaroid messenger bag umami lomo kinfolk squid asymmetrical lyft knausgaard. Cronut offal pug keytar.",
+                                 description: 'Microdosing locavore semiotics ethical lo-fi freegan salvia tumblr YOLO. Jianbing truffaut actually seitan humblebrag, 8-bit church-key yr',
+                                 banner_image_url: 'https://images.pexels.com/photos/62689/pexels-photo-62689.jpeg?w=940&h=650&auto=compress&cs=tinysrgb')
 
