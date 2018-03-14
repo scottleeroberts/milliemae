45400df27ca1cf9e174d100d4242f961c106532a
Scott <scott.lee.roberts@gmail.com>
2018-03-13 21:44:38 -0400

    Allow editing of ProductLinks

---
+++ app/controllers/designers/product_links_controller.rb
@@ -0,0 +1,53 @@
+class Designers::ProductLinksController < DesignerController
+
+  def index
+    @product_links = ProductLink.all
+  end
+
+  def new
+    @product_link = ProductLink.new
+  end
+
+  def edit
+    @product_link = ProductLink.find(params.fetch(:id))
+  end
+
+  def create
+    load_post
+    @product_link = ProductLink.new(product_link_params.merge(post_id: @post.id))
+
+    if @product_link.save
+      flash[:success] = 'Product Link Successfully added'
+      render :create
+    else
+      flash[:danger] = @product_link.pretty_errors
+      render :new
+    end
+  end
+
+  def update
+    @product_link = ProductLink.find(params.fetch(:id))
+
+    if @product_link.update(product_link_params)
+      render :update
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product_link = ProductLink.find(params.fetch(:id))
+    @product_link.destroy!
+  end
+
+  private
+
+  def load_post
+    @post = Post.friendly.find(params[:post_id])
+  end
+
+  def product_link_params
+    params.require(:product_link).permit(:url, :description)
+  end
+end
+
