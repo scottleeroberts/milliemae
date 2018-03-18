96370420cc8e22a355e557d0468aa17bb3863711
Scott Roberts <scott.lee.roberts@gmail.com>
2018-03-17 20:07:08 -0400

    Fix oops that was stopping s3 uploads in production

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.producttion?
+  if Rails.env.production?
     storage :fog
   else
     storage :file
