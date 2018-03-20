0125cb7f435b0d6c4382b805df279baf6a039286
Scott Roberts <scott.lee.roberts@gmail.com>
2018-03-19 21:35:54 -0400

    Set custom file names for uploaded files

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -25,9 +25,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg gif png)
   end
 
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    "image.#{file.extension}" if original_filename
+  end
 end
