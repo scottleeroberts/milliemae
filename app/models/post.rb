fe9affe90d00770000fb62e606e3655d58d1f2ed
Scott Roberts <scott.lee.roberts@gmail.com>
2018-05-11 08:39:36 -0400

    Minor formatting fixes

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,17 +9,18 @@ class Post < ApplicationRecord
   acts_as_taggable
 
   friendly_id :title, use: :slugged
+
   belongs_to :designer
 
   has_many :product_links
 
   validates_presence_of :title
 
-  scope :most_recent, -> { order(published_at: :desc) }
-  scope :published, -> { where(published: true) }
-  scope :recent_paginated, -> (page) { most_recent.paginate(page: page, per_page: PER_PAGE) }
-  scope :with_tag, -> (tag) { tagged_with(tag) if tag.present? }
-  scope :list_for, -> (page, tag) { recent_paginated(page).with_tag(tag) }
+  scope :most_recent, ->{ order(published_at: :desc) }
+  scope :published, ->{ where(published: true) }
+  scope :recent_paginated, ->(page) { most_recent.paginate(page: page, per_page: PER_PAGE) }
+  scope :with_tag, ->(tag) { tagged_with(tag) if tag.present? }
+  scope :list_for, ->(page, tag) { recent_paginated(page).with_tag(tag) }
 
   def should_generate_new_friendly_id?
     title_changed?
@@ -29,7 +30,7 @@ class Post < ApplicationRecord
     if published_at.present?
       "Twirled On: #{published_at.strftime('%-b %-d, %-Y')}"
     else
-      "Not published yet"
+      'Not published yet'
     end
   end
 
