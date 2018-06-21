5cb7d2d7752d719207c9762c1fcd0ec61946cc3a
Scott Roberts <scott.lee.roberts@gmail.com>
2018-06-21 19:26:07 -0400

    Replace missing line

--- app/models/designer.rb
+++ app/models/designer.rb
@@ -4,6 +4,8 @@ class Designer < ApplicationRecord
   devise :database_authenticatable,
     :recoverable, :rememberable, :trackable, :validatable
 
+  HTTP = "http://"
+
   has_many :posts
 
   before_validation :smart_add_url_protocol
