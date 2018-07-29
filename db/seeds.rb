eb97714072662d3961b7d2b944a906087b3c5e5b
Scott Roberts <scott.lee.roberts@gmail.com>
2018-07-29 13:59:20 -0400

    Seeds support social media links

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
-Designer.create(name: 'user', email: 'user@email.com', password: 'password', password_confirmation: 'password')
-Designer.create(name: 'user22', email: 'user2@email.com', password: 'password', password_confirmation: 'password')
+Designer.create(name: 'user', email: 'user@email.com', password: 'password', password_confirmation: 'password', facebook: 'www.facebook.com', website: 'www.website.com', pinterest: 'www.pinterest.com', instagram: 'www.instagram.com', etsy: 'www.etsy.com' )
 
 Designer.first.posts << Post.new(title: 'Subway tile tumblr wayfarers keffiyeh',
                                  body: "Lorem ipsum dolor amet shoreditch lyft stumptown etsy copper mug post-ironic, pop-up normcore. Actually iPhone squid, plaid drinking vinegar synth mlkshk fashion axe brooklyn disrupt viral 8-bit. Microdosing locavore semiotics ethical lo-fi freegan salvia tumblr YOLO. Jianbing truffaut actually seitan humblebrag, 8-bit church-key yr. Gentrify chillwave jean shorts tousled health goth umami street art hoodie asymmetrical you probably haven't heard of them blog everyday carry tumeric kinfolk shaman. Prism food truck skateboard health goth art party seitan polaroid messenger bag umami lomo kinfolk squid asymmetrical lyft knausgaard. Cronut offal pug keytar.",
