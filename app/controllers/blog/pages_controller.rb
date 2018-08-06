c8969bd71317f90d675792e9adebd16eda4480a7
Scott Roberts <scott.lee.roberts@gmail.com>
2018-08-05 20:57:03 -0400

    Remove contact us link in nav, and page

--- app/controllers/blog/pages_controller.rb
+++ app/controllers/blog/pages_controller.rb
@@ -1,10 +1,4 @@
 class Blog::PagesController < BlogController
-
   def about
-
-  end
-
-  def contact
-
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,7 @@ Rails.application.routes.draw do
 
   scope module: 'blog' do
     get 'about' => 'pages#about', as: :about
-    get 'contact' => 'pages#contact', as: :contact
     get 'posts' => 'posts#index', as: :posts
     get 'posts/:id' => 'posts#show', as: :post
   end
-
 end
