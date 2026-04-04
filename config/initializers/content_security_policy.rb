449136ed94d87cebca1e041c6b72abce12a7dd3e
Scott Roberts <scott.lee.roberts@gmail.com>
2026-04-04 16:47:58 -0400

    fix: allow Google Fonts through content security policy
    Add fonts.gstatic.com to font_src and fonts.googleapis.com to
    style_src so web fonts load in production.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -7,11 +7,11 @@
 Rails.application.configure do
   config.content_security_policy do |policy|
     policy.default_src :self
-    policy.font_src    :self, :data
+    policy.font_src    :self, :data, "https://fonts.gstatic.com"
     policy.img_src     :self, :data, "https://www.gravatar.com"
     policy.object_src  :none
     policy.script_src  :self
-    policy.style_src   :self, :unsafe_inline # Tailwind inline styles + hotspot style attributes
+    policy.style_src   :self, :unsafe_inline, "https://fonts.googleapis.com"
     policy.frame_ancestors :none
     policy.base_uri    :self
     policy.form_action :self
